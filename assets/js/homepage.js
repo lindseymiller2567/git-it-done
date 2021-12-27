841fc8a76c2127cd36e454f0369fbdbc166c26e0
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2021-12-27 16:09:56 -0600

    fetched users repos using server api

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/lindseymiller2567")
+var getUserRepos = function (user) {
+    // format the github api url
+    var apiUrl = "http://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data)
+        })
+    })
 }
 
 getUserRepos()
